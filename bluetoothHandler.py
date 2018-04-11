86aa521b299f09839045779da13a13fc417d6cdf
edoardesd <edoardesd@gmail.com>
2018-04-11 16:46:30 +0200

    bluetooth rssi handling

--- bluetoothHandler.py
+++ bluetoothHandler.py
@@ -11,7 +11,7 @@ class bluetoothHandler:
 		self.mac_address = mac_address
 
 		print "start process ", self.mac_address
-		self.ping = subprocess.Popen(['./multiple_ping.sh', '1c:66:aa:cc:9a:18'], bufsize=0, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		self.ping = subprocess.Popen(['./multiple_ping.sh', '1c:66:aa:cc:9a:1a'], bufsize=0, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 		return self.ping
 
@@ -22,7 +22,23 @@ class bluetoothHandler:
 
 		print "ping ", self.mac_address
 
-		rssi = subprocess.check_output(['hcitool', 'rssi', str(self.mac_address)])
-		print rssi
+		sum_rssi = 0
+		count = 0  
+		#while True:
+
+		try:
+			rssi = subprocess.check_output(['hcitool', 'rssi', "1c:66:aa:cc:9a:1a"])#, bufsize=0, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		except subprocess.CalledProcessError as e:
+			print e.output
+
+		#print "questo rssi", int(rssi)
+			#sum_rssi += int(rssi[-2:])
+			#count += count 
+
+			#if count == 50:
+			#	avg_rssi = float(sum_rssi/count)
+			#	count = 0
+			#	sum_rssi = 0
+			
 
 		return rssi
