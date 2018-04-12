0647a473f5ea28a7cdad87f8acdd1e9414fd0268
edoardesd <edoardesd@gmail.com>
2018-04-12 14:55:26 +0200

    broker changed

--- smart_direction.py
+++ smart_direction.py
@@ -15,8 +15,8 @@ from time import sleep
 
 
 
-#broker_address = "192.168.1.74"
-broker_address = "10.0.2.15" 
+broker_address = "192.168.1.74"
+#broker_address = "10.0.2.15" 
 topic_name = "topic/rasp4/directions"
 
 def on_message(client, userdata, message):
