8cd491aa17dfa3c449ffd9c30b124fc96f96b5da
edoardesd <edoardesd@gmail.com>
2018-04-18 15:54:47 +0200

    added debug and info logging

--- thread_test.py
+++ thread_test.py
@@ -85,22 +85,22 @@ class MqttThread(threading.Thread):
 
 	def run(self):
 		print "creating client --- host: ", self.host
-		logging.debug("Mqtt thread runs")
+		logging.info("Mqtt thread runs")
 		receiver = Receiver()
 		self.client.loop_start()
-		logging.debug("Mqtt starts loop")
+		logging.info("Mqtt starts loop")
 		self.client.on_connect = receiver.on_connect
 		self.client.on_message = receiver.on_message
 
 		self.client.connect_async(self.host, 1883)
-		logging.debug("opening mqtt connection")
+		logging.info("opening mqtt connection")
 		#loop until disconnect
 		#client.loop_forever()
 			
 	
 	def stop(self):
 		self.client.disconnect()
-		logging.warning("disconnect mqtt")
+		logging.info("disconnect mqtt")
 
 ######	
 
@@ -113,52 +113,76 @@ class PingThread(threading.Thread):
 		mac_target = self.user['mac_address']
 		place_id_target = self.user['place_id']
 		timestamp_target = self.user['timestamp']
+		logging.debug("Thread %s is running", self)
+		logging.debug("Thread input params. mac: %s, place_id: %s, ts: %s", mac_target, place_id_target, timestamp_target)
 
+		logging.info("Opening map")
 		tree = ET.parse('map.xml')
 		root = tree.getroot()
 
 		direction, final = xml_parser.find_direction(root, place_id_target, rasp_id)
-		
-		print "direction: ", direction
-		print "final?", final
-		print "Hi, i'm ", mac_target, " mac address"
+		logging.debug("parsing file. direcition: %s, is_final: %s", direction, final)
+		logging.info("starting bluetooth handler")
 		print "starting ping ... "
 
 		bt = bluetoothHandler.bluetoothHandler()
 		bt.start(mac_target)
 		self.is_running = True
-
 		sum_rssi = 0 
 		count = 0
 		rssi_avg = 0
 		arrived = False
 		oor_count = 0
+		print_dir = False
 
+		logging.debug("setting up params. is_running %s, arrived %s", self.is_running, arrived)
+		logging.info("staring while loop")
 		while self.is_running:
+			logging.debug("inside while loop")
 			rssi = bt.rssi()
-			
+			logging.debug("reading rssi %s", rssi)
+
+
 			if rssi is not None:
 				if rssi == "OOR":
 					print "Out of Range"
+					logging.info("Out of range")
+					logging.debug("out of range rssi: %s", rssi)
+					print_dir = False
 					if arrived:
+						logging.debug("Arrived? %s", arrived)
 						oor_count += 1
-						if oor_count > 3:
+						if oor_count > 2:
+							logging.info("User passes the destination")
+							logging.debug("Out of range count: %s", oor_count)
 							print "users pass the destination"
 							if final:
+								logging.info("Stopping all the ping")
 								print "stop all the ping"
+								#todo!
 				else:
 					try:
 						rssi = float(rssi)
-					except ValueError:
+						#logging.debug("puntual rssi: %s", rssi)
+					except ValueError as e:
+						logging.error("Rssi concersion error %s", e)
 						print "Conversion error!"
 						break	
 
 					rssi_avg, count, sum_rssi = average_rssi(rssi, count, sum_rssi)
 					if rssi_avg is not None:
+						logging.debug("average rssi: %s", rssi)
 						position = check_proximity(rssi_avg)
-						if (position == "very near") or (position == "very near"):
+						if position < 4:
+							if not print_dir:
+								print "ARROW", direction
+								print_dir = True
+							logging.info("Printing out direction")
+						logging.debug("position is %s", position)
+						if position < 2:
 							arrived = True
-						print position, "---", rssi_avg
+							logging.info("The user is arrived to destination")
+						print "position:", position, "---", rssi_avg
 					
 					 
 
@@ -169,19 +193,18 @@ class PingThread(threading.Thread):
 
 
 def signal_handler(signal, frame):
-	logging.debug("Signal Handler arrived")
+	logging.info("Signal Handler arrived")
 	print "Exit!"
 
 	#close all the thread in thread list
 	logging.debug("the thread are: %s", t_sniffer)
 	for user in t_sniffer:
-		logging.debug("closing thread ")
-		logging.debug(user)
+		logging.debug("closing thread %s", user)
 		user.stop()
 
-	logging.debug("stopping all ping thread")
+	logging.info("stopping all ping thread")
 	t_mqtt.stop()
-	logging.debug("Stopping mqtt thread")
+	logging.info("Stopping mqtt thread")
 	try:
 		killall_ping = subprocess.check_output(['killall', 'l2ping'], stderr=subprocess.PIPE)
 		logging.debug("Closing l2ping process %s", killall_ping)
@@ -191,7 +214,7 @@ def signal_handler(signal, frame):
 
 	
 
-	logging.debug("Closing the program")
+	logging.info("Closing the program")
 	sys.exit(0)
 
 def args_parser():
@@ -213,7 +236,7 @@ def args_parser():
 			logging.debug("help")
 			print "TODO how to use"
 			#usage()
-			logging.debug("exit program")
+			logging.info("exit program")
 			sys.exit(2)
 		elif opt in ('-r', '--rasp'):
 			rasp = 1
@@ -222,7 +245,7 @@ def args_parser():
 			#usage()
 			logging.warning("some error in params ",params)
 			print "Exit.. TODO how to use"
-			logging.debug("exit program")
+			logging.info("exit program")
 			sys.exit(2)
 
 def average_rssi(rssi, count, sum_rssi):
@@ -238,22 +261,35 @@ def average_rssi(rssi, count, sum_rssi):
 
 def check_proximity(rssi):
 	if rssi > 0.5:
-		return "very very near"
-	if rssi > -1.0 and rssi <=0.5:
-		return "very near"
-	if rssi > -10 and rssi <=-1.0:
-		return "near"
-	if rssi > -20 and rssi <=-10:
-		return "visible"
-	if rssi <= -20:
-		return "in range"
+		position = "very very near"
+		logging.debug(position)
+		return 0
+	elif rssi > -1.0 and rssi <=0.5:
+		position = "very near"
+		logging.debug(position)
+		return 1
+	elif rssi > -10 and rssi <=-1.0:
+		position = "near"
+		logging.debug(position)
+		return 2
+	elif rssi > -20 and rssi <=-10:
+		position = "visible"
+		logging.debug(position)
+		return 3
+	elif rssi <= -20:
+		position = "in range"
+		logging.debug(position)
+		return 4
+	else:
+		return 9
 
 #### MAIN ####
 if __name__ == "__main__":
-	logging.debug("_____________________________")
+	logging.info("_____________________________")
+	logging.info("SM4RT_D1R3CT10Nz v0.3 thread")
 	signal.signal(signal.SIGINT, signal_handler)
 	print "SM4RT_D1R3CT10Nz v0.3 thread", rasp_id
-	logging.debug("Starting main...")
+	logging.info("Starting main...")
 
 	args_parser()
 
@@ -261,11 +297,11 @@ if __name__ == "__main__":
 		broker_address = broker_address_cluster
 		logging.debug("Set broker address in rasp mode: "+ broker_address)
 
-	logging.debug("the broker_address is "+broker_address)
+	logging.info("the broker_address is "+broker_address)
 
 	t_mqtt = MqttThread(broker_address)
 	t_mqtt.setDaemon(True)
-	logging.debug("Setting up the mqtt thread")
+	logging.info("Setting up the mqtt thread")
 	
 	global t_sniffer
 	t_sniffer = []
@@ -276,7 +312,7 @@ if __name__ == "__main__":
 	while True:
 		if not q.empty():
 			item = q.get()
-			logging.debug("A new message is arrived. ID %s" ,item['id'])
+			logging.info("A new message is arrived. ID %s" ,item['id'])
 			logging.info(item)
 			print "new message is arrived... ID is:", item['id']
 			user = PingThread(item)
