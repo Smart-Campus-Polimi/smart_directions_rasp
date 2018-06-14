e31b55940419777ef536c9f0f6d4f8e3f5afd1c0
edoardesd <edoardesd@gmail.com>
2018-06-14 11:58:54 +0200

    test triangles

---
+++ pyglet_window.py
@@ -0,0 +1,22 @@
+from pyglet.gl import *
+
+class Triangle:
+	def __init__(self):
+		self.vertices = pyglet.graphics.vertex_list(3, ('v3f', [-0.5,-0.5,0.0, 0.5,-0.5,0.0, 0.0,0.5,0.0]),
+														('c3B', [100,200,220, 200,110,192, 129,200,238]))
+
+class MyWindow(pyglet.window.Window):
+		def __init__(self, *args, **kwargs):
+			super(MyWindow, self).__init__(*args, **kwargs)
+			self.set_minimum_size(400,300)
+
+			self.triangle = Triangle()
+		def on_draw(self):
+			self.triangle.vertices.draw(GL_TRIANGLES)
+			
+
+
+if __name__ == "__main__":
+	window = MyWindow(1280, 720, "test directions")
+	window.on_draw()
+	pyglet.app.run()
