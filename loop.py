c757a81680fd62f5d738e652f483a6cbe63187e7
Edoardo Longo <edoardesd@gmail.com>
2018-10-04 17:54:19 +0200

    Delete loop.py

--- loop.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-while 1:
-	try:
-		rssi = subprocess.check_output(['hcitool', 'rssi', '1c:66:aa:cc:9a:18'])	 
-		print rssi
-	except subprocess.CalledProcessError as e:
-		print e.output
